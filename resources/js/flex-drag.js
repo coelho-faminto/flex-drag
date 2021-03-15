3957555d3899c789ed887856d4260143bc7151cf
coelho-faminto <coelho@nostracasa.org>
2021-03-15 17:00:31 -0300

    added rectangles to show where box is gonna be placed

--- resources/js/flex-drag.js
+++ resources/js/flex-drag.js
@@ -7,10 +7,32 @@ class FlexDrag {
 
         const elRect = ev.target.getBoundingClientRect();
 
-        if (ev.x > elRect.left + elRect.width / 2) {
-            //console.log("insert after...");
+        this.clear_canvas();
+
+        if (ev.x > elRect.x + (elRect.width * 2) / 3) {
+            this.draw_border(
+                elRect.x + (elRect.width * 2) / 3,
+                elRect.y,
+                elRect.width / 3,
+                elRect.height,
+                "#0000aa"
+            );
+        } else if (ev.x > elRect.x + elRect.width / 3) {
+            this.draw_border(
+                elRect.x + elRect.width / 3,
+                elRect.y,
+                elRect.width / 3,
+                elRect.height,
+                "#00aa00"
+            );
         } else {
-            //console.log("insert before...");
+            this.draw_border(
+                elRect.x,
+                elRect.y,
+                elRect.width / 3,
+                elRect.height,
+                "#aa0000"
+            );
         }
     }
 
@@ -41,6 +63,7 @@ class FlexDrag {
         ev.preventDefault();
 
         ev.target.style.outline = "0";
+        this.clear_canvas();
 
         const element_id = ev.dataTransfer.getData("text");
         const element = document.querySelector(
@@ -53,43 +76,83 @@ class FlexDrag {
             const elRect = ev.target.getBoundingClientRect();
 
             console.log(elRect);
-            console.log(ev.target);
+            console.log(ev);
 
-            if (ev.x > elRect.left + elRect.width / 2) {
-                ev.target.insertAdjacentElement("beforebegin", element);
-
-                console.log("before..");
-            } else {
+            if (ev.x > elRect.x + (elRect.width * 2) / 3) {
                 ev.target.insertAdjacentElement("afterend", element);
-
-                console.log("after...");
+            } else if (ev.x > elRect.x + elRect.width / 3) {
+                ev.target.insertAdjacentElement("beforeend", element);
+            } else {
+                ev.target.insertAdjacentElement("beforebegin", element);
             }
+
+            ev.stopPropagation();
         }
+    }
+
+    toggle_edit_mode() {
+        document.body.classList.toggle("fdrag-edit-mode");
     }
 
     on_keydown(ev) {
         if (ev.ctrlKey && ev.keyCode == 0x51) {
             //console.log("pressed CTRL+Q");
-            document.body.classList.toggle("fdrag-edit-mode");
+            this.toggle_edit_mode();
         }
     }
 
+    resize_canvas() {
+        if (!this.canvas) return;
+
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+    }
+
+    clear_canvas() {
+        const ctx = this.canvas ? canvas.getContext("2d") : false;
+
+        if (!ctx) return;
+
+        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    draw_border(x, y, w, h, color) {
+        const ctx = this.canvas ? canvas.getContext("2d") : false;
+
+        if (!ctx) return;
+
+        ctx.beginPath();
+
+        ctx.fillStyle = "rgba(0, 0, 0, 0.1666)";
+        ctx.fillRect(x + 2, y + 2, w - 4, h - 4);
+        ctx.strokeStyle = color;
+        ctx.lineWidth = 1;
+        //ctx.stroke();
+    }
+
     hook_elements() {
+        const canvas = document.querySelector("#canvas");
+        this.canvas = canvas ? canvas : false;
+
+        window.addEventListener("resize", this.resize_canvas);
+        this.resize_canvas();
+
+        //this.draw_border(10, 10, 100, 200);
+
         var elements = document.querySelectorAll(".fdrag-box");
 
         elements.forEach((v) => {
-            v.addEventListener("dragover", this.on_dragover);
+            v.addEventListener("dragover", this.on_dragover.bind(this));
             v.addEventListener("dragleave", this.on_dragleave);
             v.addEventListener("dragenter", this.on_dragenter);
             v.addEventListener("dragstart", this.on_dragstart);
-            v.addEventListener("drop", this.on_drop);
+            v.addEventListener("drop", this.on_drop.bind(this));
         });
 
-        document.addEventListener("keydown", this.on_keydown);
+        document.addEventListener("keydown", this.on_keydown.bind(this));
     }
 
     constructor() {
-        console.log("test");
         this.hook_elements();
     }
 }
